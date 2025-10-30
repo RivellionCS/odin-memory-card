ef3a751eeb21577c000423956432ad58b5ec4b17
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-10-30 03:29:30 -0300

    feat: add footer component function

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+export { Footer };
+
+function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/RivellionCS/odin-memory-card"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Source Code
+      </a>
+      <a
+        href="https://github.com/RivellionCS"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Rivellion
+      </a>
+    </footer>
+  );
+}
